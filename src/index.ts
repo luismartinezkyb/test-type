ef7c08fd351429e2c4bfe7bd30acae091dcc4c68
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-06-18 19:05:51 -0600

    cambios para establecer types de Request

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,30 @@ app.use(express.json());
 
 app.use('/api/diary',diaryRoute);
 
+// import express, {Express, Response, Request} from 'express';
+
+
+
+// const app:Express = express();
+
+// //config
+// const PORT = process.env.PORT || 3001;
+
+// //
+// app.use(express.json());
+// //middlewares
+
+
+// //routes
+
+// app.get('/api', (req:Request, res:Response)=>{
+// 	console.log('Testing..');
+// 	res.send('Hola mundo');
+// });
+// app.get('/hola', (req:Request, res:Response)=>{
+// 	console.log('Testing..');
+// 	res.send('AHORA TESTING')
+// });
 //listen
 
 app.listen(PORT, ()=>console.log(`Your app is listening on ${PORT}`));
